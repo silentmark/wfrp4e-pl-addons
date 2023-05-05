1660d46d2c18047e3de9ae8747d943ba473052b5
Marek Kembrowski <marek.kembrowski@se.gt.com>
2023-05-05 16:10:36 +0200

    experimenting with tzeentch influence

--- scripts/windsofmagic.js
+++ scripts/windsofmagic.js
@@ -5,7 +5,6 @@ Hooks.on("createCombat", async function (combat) {
         const templateData = {};
         templateData.winds = [];
         for(let i = 0; i < winds.length; i++) {
-
             templateData.winds[i] = {};
             templateData.winds[i].wind = winds[i];
             templateData.winds[i].modifier = 0;
@@ -20,16 +19,18 @@ Hooks.on("createCombat", async function (combat) {
                 label: "Rzut",
                 callback: async (dlg) => { 
                     const inputs = $('input[name]', dlg);
-                    const winds = [];
+                    const winds = {}
+                    winds.modifier = [];
                     let message = '';
                     for (let i = 0; i < inputs.length; i++) {
                         const input = inputs[i];
                         const wind =  input.attributes['name'].value;
                         const modifier = Number.parseInt(input.value);
                         const result = Number.parseInt( (await game.wfrp4e.tables.rollTable('winds', { hideDSN: true, modifier: modifier })).result);
-                        winds.push({wind: wind, modifier: result});
+                        winds.modifier.push({wind: wind, modifier: result});
                         message += `<tr><td><strong>${wind}</strong>:</td><td>${result > 0 ? '+' + result : result}</td></tr>`
                     }
+                    winds.tzeentch = $('input[name="tzeentchInfluence"]', dlg)[0].checked;
                     await combat.setFlag('wfrp4e-pl-addons', 'winds', winds);
                     await ChatMessage.create({ content: `<p><strong>Zmienne Wiatry magii wpływają na splatanie w następujący sposób:</strong><br/><table>${message}</table></p>` });
                 }
@@ -58,10 +59,10 @@ Hooks.on("updateCombat", async function (combat, updateData) {
             let wind = skill.name.substring(skill.name.indexOf("(") + 1, skill.name.indexOf(")"));
             let winds = combat.flags['wfrp4e-pl-addons']['winds'];
             if (winds) {
-                let modifier = winds.find(x=> x.wind == wind).modifier;
+                let modifier = winds.modifier.find(x=> x.wind == wind).modifier;
                 if (modifier != 0) {
                     let effect = actor.effects.find(x => x.label == 'Wiatry Magii (' + wind + ')');
-                    if(!effect) {
+                    if (!effect) {
                         effect = {
                             label: 'Wiatry Magii (' + wind + ')',
                             icon: "systems/wfrp4e/ui/buttons/d10.webp",
@@ -80,6 +81,81 @@ Hooks.on("updateCombat", async function (combat, updateData) {
                             }
                         }
                         await actor.createEmbeddedDocuments("ActiveEffect", [effect])
+                    }
+                }
+                if (winds.tzeentch) {
+                    let effect = actor.effects.find(x => x.label == 'Złowrogie Wpływy Tzeentcha (Czarowanie)');
+                    if (!effect) {
+                        effect = {
+                            label: 'Złowrogie Wpływy Tzeentcha (Czarowanie)',
+                            icon: "modules/wfrp4e-core/icons/spells/undivided.png",
+                            transfer: false,
+                            flags: {
+                                wfrp4e: {
+                                    "effectApplication": "actor",
+                                    "effectTrigger": "rollCastTest",
+                                    "script": `
+                                        if (args.test.result.roll.toString() == '99') {
+                                            let demonTokenId = (await warpgate.spawn("Demonetka Slaanesha"))[0];
+                                            let demonToken = game.canvas.tokens.get(demonTokenId);
+                                            let caster = this.actor;
+                                            setTimeout(async function () {
+                                                new Sequence()
+                                                .effect()
+                                                    .file("modules/jb2a_patreon/Library/Generic/Magic_Signs/AbjurationCircleComplete_02_Dark_Pink_800x800.webm")
+                                                    .atLocation(demonToken)
+                                                    .scaleToObject(2.5)
+                                                    .randomRotation()
+                                                .play();
+                                            }, 200);
+                                            ChatMessage.Create({content: "<a class='table-click fumble-roll' title='Złowrogie Wpływy Tzeentcha' data-table='majormis'><i class='fas fa-list'></i>Poważna Manifestacja Chaosu</a>"})
+                                            //TODO: add effect for one turn/ 
+                                        }
+                                        else if (args.test.result.roll.toString().split('').reverse()[0] == '9') {
+                                            ChatMessage.Create({content: "<a class='table-click fumble-roll' title='Złowrogie Wpływy Tzeentcha' data-table='minormis'><i class='fas fa-list'></i>Pomniejsza Manifestacja Chaosu</a>"});
+                                            //TODO: add effect for one turn/ 
+                                        }
+                                `
+                                }
+                            }
+                        }
+                        await actor.createEmbeddedDocuments("ActiveEffect", [effect])
+                    }                    
+                    effect = actor.effects.find(x => x.label == 'Złowrogie Wpływy Tzeentcha (Splatanie)');
+                    if (!effect) {
+                        effect = {
+                            label: 'Złowrogie Wpływy Tzeentcha (Splatanie)',
+                            icon: "modules/wfrp4e-core/icons/spells/undivided.png",
+                            transfer: false,
+                            flags: {
+                                wfrp4e: {
+                                    "effectApplication": "actor",
+                                    "effectTrigger": "rollChannellingTest",
+                                    "script": `                                    
+                                        if (args.test.result.roll.toString() == '99') {
+                                            let demonTokenId = (await warpgate.spawn("Demonetka Slaanesha"))[0];
+                                            let demonToken = game.canvas.tokens.get(demonTokenId);
+                                            let caster = this.actor;
+                                            setTimeout(async function () {
+                                                new Sequence()
+                                                .effect()
+                                                    .file("modules/jb2a_patreon/Library/Generic/Magic_Signs/AbjurationCircleComplete_02_Dark_Pink_800x800.webm")
+                                                    .atLocation(demonToken)
+                                                    .scaleToObject(2.5)
+                                                    .randomRotation()
+                                                .play();
+                                            }, 200);
+                                            ChatMessage.Create({content: "<a class='table-click fumble-roll' title='Złowrogie Wpływy Tzeentcha' data-table='majormis'><i class='fas fa-list'></i>Poważna Manifestacja Chaosu</a>"})
+                                            //TODO: add effect for one turn/ 
+                                        }
+                                        else if (args.test.result.roll.toString().split('').reverse()[0] == '9') {
+                                            ChatMessage.Create({content: "<a class='table-click fumble-roll' title='Złowrogie Wpływy Tzeentcha' data-table='minormis'><i class='fas fa-list'></i>Pomniejsza Manifestacja Chaosu</a>"});
+                                            //TODO: add effect for one turn/ 
+                                        }
+                                `
+                                }
+                            }
+                        }
                     }
                 }
             }
